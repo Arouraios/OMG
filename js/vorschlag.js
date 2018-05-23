2f2cbf56780cfa0b25950a7f7107ad2236e3edb1
Arouraios <lao_shu@gmx.de>
2018-05-23 12:00:09 +0200

    guess what changed

--- js/vorschlag.js
+++ js/vorschlag.js
@@ -2,5 +2,11 @@ function addVorschlag()
 {
    var vorschlag = document.getElementById("vorschlag").value
     console.log(vorschlag);
-	document.getElementById("vorschlag").clear;
+	clearContent(document.getElementById("vorschlag"));
+}
+
+function clearContent(element){
+	if(element.value=='Hast du gute Ideen für Themen in der Zukunft? Würdest du gerne etwas ändern? Dann schreib es einfach hier rein'){
+		element.value='';
+	}
 }
